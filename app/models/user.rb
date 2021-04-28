6669f3cb07207d44b9608a21f84eaa076ae9bd39
Vishal Soni <vishalsbl123@gmail.com>
2021-04-28 11:07:35 +0530

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	
 	has_secure_password
+	
 	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_04_26_133619) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
